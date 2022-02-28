c2c5110af41116a9fcffe8847486649f1c5144e1
unknown <lopez1432.al@gmail.com>
2022-02-28 13:53:42 -0800

    server JS rework

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, model, Types } = require("mongoose");
+const { type } = require("os");
+const dateFormat = require("../utils/dateFormat"); 
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+},
+{
+            toJSON: {
+                getters: true
+            },
+            id: false
+        }
+);
+
+const User = model("Reaction", ReactionSchema);
